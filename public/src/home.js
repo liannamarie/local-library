d8c2623b1b4f93df244d407e9d268902b381558a
Lianna Santiago <77291562+liannamarie@users.noreply.github.com>
2021-10-13 21:41:57 -0400

    Update home.js

--- public/src/home.js
+++ public/src/home.js
@@ -1,20 +1,34 @@
-function getTotalBooksCount(books) {}
+function getMostPopularBooks(books) {
+ return books
+  .map((book) => {
+   return { name: book.title, count: book.borrows.length };
+  })
+  .sort((a, b) => (a.count < b.count ? 1 : -1))
+  .slice(0, 5);
+}
 
-function getTotalAccountsCount(accounts) {}
-
-function getBooksBorrowedCount(books) {}
-
-function getMostCommonGenres(books) {}
-
-function getMostPopularBooks(books) {}
-
-function getMostPopularAuthors(books, authors) {}
+function getMostPopularAuthors(books, authors) {
+ let result = [];
+ authors.forEach((author) => {
+  let theAuthor = {
+   name: `${author.name.first} ${author.name.last}`,
+   count: 0
+  };
+  books.forEach((book) => {
+   if (book.authorId === author.id) {
+    theAuthor.count += book.borrows.length;
+   }
+  });
+  result.push(theAuthor);
+ });
+ return result.sort((a, b) => b.count - a.count).slice(0, 5);
+}
 
 module.exports = {
-  getTotalBooksCount,
-  getTotalAccountsCount,
-  getBooksBorrowedCount,
-  getMostCommonGenres,
-  getMostPopularBooks,
-  getMostPopularAuthors,
+ getTotalBooksCount,
+ getTotalAccountsCount,
+ getBooksBorrowedCount,
+ getMostCommonGenres,
+ getMostPopularBooks,
+ getMostPopularAuthors
 };
